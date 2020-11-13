a47713f97c1322fa6b19ba8e8bb16b56c4b2f460
PouletRaPHOO <raphael.herzlich@icloud.com>
2020-11-13 13:37:15 +0100

    Jai tout cassé

--- popup.js
+++ popup.js
@@ -1,2 +1,22 @@
 $(function(){
+
+    chrome.storage.sync.get(['Adresse','Password','Auth'], function(authentifications){
+        console.log(authentifications.Adresse)
+        $('#url').val(authentifications.Adresse),
+        $('#Auth').val(authentifications.Auth),
+        $('#Password').val(authentifications.Password);
+    })
+
+
+    $('#subAuth').click(function(){
+        console.log("test")
+        var URL = $('#url').val();
+        var Auth = $('#Auth').val();
+        var Password = $('#Password').val();
+        if(URL&&Password&&Auth) {
+            chrome.storage.sync.set({'Adresse':URL, 'Auth':Auth,'Password':Password}, function(){
+                close();
+            })
+        }
+    })
 })
