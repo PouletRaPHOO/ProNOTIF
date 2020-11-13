f37d058efbf1c546c387c625ad2cb2f041399059
PouletRaPHOO <raphael.herzlich@icloud.com>
2020-11-13 19:46:52 +0100

    Ajout du login + test login

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
-
-chrome.storage.sync.get(['Adresse','Password','Auth'], function(authentifications){
-    console.log(authentifications.Adresse)
-    Adresse = authentifications.Adresse;
-    Auth = authentifications.Auth;
-    Password =authentifications.Password;
-})
-console.log("pouet")
+function checkId() {
+    chrome.storage.sync.get(['Adresse','Password','Auth'], function(authentifications){
+        console.log(authentifications.Adresse)
+        Adresse = authentifications.Adresse;
+        Auth = authentifications.Auth;
+        Password =authentifications.Password;
+    })
+    setTimeout(checkId,60000);
+}
+setTimeout(checkId,1000)
--- popup.js
+++ popup.js
@@ -14,8 +14,25 @@ $(function(){
         var Auth = $('#Auth').val();
         var Password = $('#Password').val();
         if(URL&&Password&&Auth) {
-            chrome.storage.sync.set({'Adresse':URL, 'Auth':Auth,'Password':Password}, function(){
-                close();
+            chrome.storage.sync.set({'Adresse':URL, 'Auth':Auth,'Password':Password}, async function(test) {
+                testUrl = `https://us-central1-pronote-login-api-ext.cloudfunctions.net/app/pronote/lastmessage/?url=${test.Adresse}&username=${test.Auth}&password=${test.Password}`
+                response= await jQuery.ajax(TestUrl)
+                if (typeof response === Object) {
+                    var notifLogged = {
+                        type: 'basic',
+                        iconUrl ='logos/logo128.png',
+                        title ='Bien Connecté',
+                        message ='Vous etes bien connecté sur la session Pronote de (à mettre plus tard avec une nouvelle URL)'
+                    };
+                    chrome.notifications.create("Logged",notifLogged)
+                } else {
+                    var notifLogged = {
+                        type: 'basic',
+                        iconUrl ='logos/logo128.png',
+                        title ='Erreur de Connexion',
+                        message ='Uh oh Looks like you got the Wrong passwords'
+                    };
+                }
             })
         }
     })
