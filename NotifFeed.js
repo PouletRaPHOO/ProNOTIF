91d7df639a6e4b7fcd8ade2a648b056d2724836a
PouletRaPHOO <raphael.herzlich@icloud.com>
2020-11-14 22:57:57 +0100

    Dernier push avant de "dodo"

--- NotifFeed.js
+++ NotifFeed.js
@@ -4,30 +4,28 @@ $(function(){
     joursSemaine = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
     const parse = (date) => {
         date = new Date(date)
-        return date
-    }
+        return date}
     const jourSem = (date) => {
         let i=0
-        while (i!== date.getDay()&&i>6) {
+        Jour=date.getDay()
+        while (i!== Jour && i<6) {
             date=joursSemaine[i]
-        }
-        return date
-    }
+            i++}
+        return date}
     const moisAn = (date) => {
         let i=0
-        while (i!== date.getMonth()) {
+        Mois=date.getMonth()
+        while (i!== Mois && i<11) {
             date=mois[i]
-        }
-    }
-
-
+            i++}
+        return date}
     chrome.storage.sync.get(["0","1","2","3"], function(Distribution){
         for (let i=0;i<Object.keys(Distribution).length;i++) {
             console.log(Distribution[i])
             $(`#msg${i}`).html(Distribution[i].content)
             $(`#title${i}`).text(Distribution[i].title)
             dateOriginal =parse(Distribution[i].date)
-            dateMsg = `${jourSem(dateOriginal)}${dateOriginal.getDate}${moisAn(dateOriginal)}${dateOriginal.getYear}`
+            dateMsg = `${jourSem(dateOriginal)} ${dateOriginal.getDate()} ${moisAn(dateOriginal)} ${dateOriginal.getYear()}`
             $(`#date${i}`).text(dateMsg)
         }
     })
